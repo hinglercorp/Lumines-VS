850f965fba9afc47d3ade0707397f2fb44a315f1
wasomereal <110752015+wasomereal@users.noreply.github.com>
2025-08-10 20:32:26 -0700

    Assets and scripts folder for organization

---
+++ Scripts/NewScript.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class NewScript : Node {
+	public override void _Ready() {
+		// called on initialization
+
+	}
+	public override void _Process(double delta) {
+		// called every frame
+		
+	}
+}
